d3c7cf85b660f72a1fd171a8bd47e3149d7e89fb
Leonardo Capogna <leonardocapogna@gmail.com>
2021-01-12 17:56:41 +0100

    background image to fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,4 +3,7 @@
 let mix = require('laravel-mix');
 
 // mix.js('src/app.js', 'dist').setPublicPath('dist');
-mix.sass('src/app.scss', 'css');
+
+mix.sass('src/app.scss', 'css').options({
+    processCssUrls: false // questa opzione fa in modo da non modificare i percorsi delle immagini
+});
